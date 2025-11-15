37943888aa3bdd9103c31a67de13a308b9e120b9
Nguyễn Duy <ducduy10k@gmail.com>
2025-11-15 23:39:09 +0700

    add jenkin

---
+++ Test Suites/Open App.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Open App</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>a76c989d-6cf2-42e6-9133-2fd0a5925426</testSuiteGuid>
+</TestSuiteEntity>
